0c189397663cf7292952a9d676d28bd6fa0052aa
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2024-06-20 22:14:25 -0300

    AULA 01

---
+++ 01_Conceitos/Projeto01.Conceitos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //COMENTARIO DE UMA LINHA
+
+            /*
+             * VARIAS LINHAS. 
+             * 
+             * 
+             */
+            //CTRL + S Salvar.
+
+            //CTRL + K + D 
+            Console.WriteLine("Programando com C#");
+            Console.WriteLine("Periodo: 20/06/2024 a 25/06/2024");
+            Console.WriteLine("Noturno");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 01_Conceitos/Projeto02.Tipos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis.
+            //crie uma variavel e atribua o nome Lucas.
+            string nome = "Lucas";
+
+            //escreva na tela.
+            Console.WriteLine(nome);
+
+            //aguarde uma ação.
+            Console.ReadKey();
+        }
+    }
+}
