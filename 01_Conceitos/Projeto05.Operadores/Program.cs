1b975d2130227793f076e387778f3739ba839b1b
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2024-06-26 22:10:00 -0300

    AULA 03

---
+++ 01_Conceitos/Projeto05.Operadores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto05.Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operador de Divisao Inteira
+            Console.Write("Informe um número inteiro: ");
+            //int.Parse convert string para inteiro.
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe um número inteiro: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            /*  / = divisao
+                + = soma
+                - = diminui
+                * = multiplicação
+                % = módulo (resto da divisao)
+            */
+
+            //retorno inteiro
+            //double resultado = n1 / n2;
+
+            //retorno decimais
+            double resultado = (double)n1 / n2;
+
+            //resto da divisao
+            //resto da divisao de n1 por n2.
+            int resto = n1 % n2;
+
+            Console.Write($"Divisão inteira: {resultado}");
+            Console.Write($"Resto da divisao inteira: {resto}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ 01_Conceitos/Projeto06.OperadorTernario/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto06.OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicitar a idade de uma pessoa.
+            Console.Write("Informe sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            //if ternario
+            //idade < 18 condicao ?
+            //"Menor Idade" - (true)
+            //"Maior de Idade" - (false)
+
+            string resposta = idade < 18 ?
+                "Menor Idade" : "Maior de Idade";
+
+            Console.WriteLine($"Você é {resposta}");
+            Console.WriteLine(); //linha em branco.
+
+            //Verificar se o ano é bissexto
+            Console.Write("Informe um ano");
+            //Dias em fevereiro.
+            //se a divisao do ano por 4 o
+            //resto for 0 entao o  ano é
+            // bissexto;
+            int ano = int.Parse(Console.ReadLine());
+            //Calcular dias de feveiro baseando-se
+            //se o ano é bissexto.
+            int diasFevereiro 
+                = (ano % 4) == 0 ? 29 : 28;
+
+            Console.WriteLine($"O ano {ano}" +
+                $"possui {diasFevereiro} dias em" +
+                $"fevereiro");
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
